98d1c362ef0173670919309d9972b5000e206ff7
Çağrı Aldemir <cagri.aldemir@hotmail.com>
2018-12-22 02:12:08 +0300

    Add files via upload

---
+++ BackupAndRestoreMSSQL/FrmMain.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnConnectAndGetTableNames = new System.Windows.Forms.Button();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtUserName = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtBackupPath = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtServerName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.chkYedektenGeriYukle = new System.Windows.Forms.CheckBox();
+            this.chkYedekle = new System.Windows.Forms.CheckBox();
+            this.btnGozat = new System.Windows.Forms.Button();
+            this.fBrwDialog = new System.Windows.Forms.FolderBrowserDialog();
+            this.SuspendLayout();
+            // 
+            // btnConnectAndGetTableNames
+            // 
+            this.btnConnectAndGetTableNames.Font = new System.Drawing.Font("Microsoft Sans Serif", 6.7F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnConnectAndGetTableNames.Location = new System.Drawing.Point(117, 138);
+            this.btnConnectAndGetTableNames.Name = "btnConnectAndGetTableNames";
+            this.btnConnectAndGetTableNames.Size = new System.Drawing.Size(180, 23);
+            this.btnConnectAndGetTableNames.TabIndex = 21;
+            this.btnConnectAndGetTableNames.Text = "BAĞLAN VE YEDEKLE";
+            this.btnConnectAndGetTableNames.UseVisualStyleBackColor = true;
+            this.btnConnectAndGetTableNames.Click += new System.EventHandler(this.btnConnectAndGetTableNames_Click);
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(117, 86);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(180, 20);
+            this.txtPassword.TabIndex = 20;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(70, 89);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(41, 13);
+            this.label5.TabIndex = 19;
+            this.label5.Text = "ŞİFRE:";
+            // 
+            // txtUserName
+            // 
+            this.txtUserName.Location = new System.Drawing.Point(117, 60);
+            this.txtUserName.Name = "txtUserName";
+            this.txtUserName.Size = new System.Drawing.Size(180, 20);
+            this.txtUserName.TabIndex = 18;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(25, 63);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(86, 13);
+            this.label4.TabIndex = 17;
+            this.label4.Text = "KULLANICI ADI:";
+            // 
+            // txtBackupPath
+            // 
+            this.txtBackupPath.Location = new System.Drawing.Point(117, 112);
+            this.txtBackupPath.Name = "txtBackupPath";
+            this.txtBackupPath.Size = new System.Drawing.Size(180, 20);
+            this.txtBackupPath.TabIndex = 16;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(33, 115);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(78, 13);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "YEDEK YOLU:";
+            // 
+            // txtServerName
+            // 
+            this.txtServerName.Location = new System.Drawing.Point(117, 34);
+            this.txtServerName.Name = "txtServerName";
+            this.txtServerName.Size = new System.Drawing.Size(180, 20);
+            this.txtServerName.TabIndex = 14;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 37);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(99, 13);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "SUNUCU ADRESİ:";
+            // 
+            // chkYedektenGeriYukle
+            // 
+            this.chkYedektenGeriYukle.AutoSize = true;
+            this.chkYedektenGeriYukle.Location = new System.Drawing.Point(117, 5);
+            this.chkYedektenGeriYukle.Name = "chkYedektenGeriYukle";
+            this.chkYedektenGeriYukle.Size = new System.Drawing.Size(151, 17);
+            this.chkYedektenGeriYukle.TabIndex = 24;
+            this.chkYedektenGeriYukle.Text = "YEDEKTEN GERİ YÜKLE";
+            this.chkYedektenGeriYukle.UseVisualStyleBackColor = true;
+            this.chkYedektenGeriYukle.CheckedChanged += new System.EventHandler(this.chkYedektenGeriYukle_CheckedChanged);
+            // 
+            // chkYedekle
+            // 
+            this.chkYedekle.AutoSize = true;
+            this.chkYedekle.Checked = true;
+            this.chkYedekle.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.chkYedekle.Location = new System.Drawing.Point(36, 5);
+            this.chkYedekle.Name = "chkYedekle";
+            this.chkYedekle.Size = new System.Drawing.Size(75, 17);
+            this.chkYedekle.TabIndex = 25;
+            this.chkYedekle.Text = "YEDEKLE";
+            this.chkYedekle.UseVisualStyleBackColor = true;
+            this.chkYedekle.CheckedChanged += new System.EventHandler(this.chkYedekle_CheckedChanged);
+            // 
+            // btnGozat
+            // 
+            this.btnGozat.Location = new System.Drawing.Point(303, 112);
+            this.btnGozat.Name = "btnGozat";
+            this.btnGozat.Size = new System.Drawing.Size(29, 20);
+            this.btnGozat.TabIndex = 26;
+            this.btnGozat.Text = "...";
+            this.btnGozat.UseVisualStyleBackColor = true;
+            this.btnGozat.Click += new System.EventHandler(this.btnGozat_Click);
+            // 
+            // fBrwDialog
+            // 
+            this.fBrwDialog.ShowNewFolderButton = false;
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(351, 173);
+            this.Controls.Add(this.btnGozat);
+            this.Controls.Add(this.chkYedekle);
+            this.Controls.Add(this.chkYedektenGeriYukle);
+            this.Controls.Add(this.btnConnectAndGetTableNames);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtUserName);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtBackupPath);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtServerName);
+            this.Controls.Add(this.label2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "FrmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "MSSQL Database Backup/Restore";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnConnectAndGetTableNames;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtUserName;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtBackupPath;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtServerName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckBox chkYedektenGeriYukle;
+        private System.Windows.Forms.CheckBox chkYedekle;
+        private System.Windows.Forms.Button btnGozat;
+        private System.Windows.Forms.FolderBrowserDialog fBrwDialog;
+    }
+}
+
---
+++ BackupAndRestoreMSSQL/FrmMain.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class FrmMain : Form
+    {
+        OleDbConnection cnn;
+        List<string> fileNameList;
+
+        public FrmMain()
+        {
+            InitializeComponent();
+            fileNameList = new List<string>();
+        }
+
+        private string GetFileListFromDirectory()
+        {
+            string error = "";
+            try
+            {
+                DirectoryInfo d = new DirectoryInfo(txtBackupPath.Text);//Assuming Test is your Folder
+                FileInfo[] files = d.GetFiles("*.bak"); //Getting Text files
+                foreach (FileInfo file in files)
+                {
+                    fileNameList.Add(file.Name);
+                }
+            }
+            catch (Exception e)
+            {
+                error = e.Message;
+            }
+            return error;
+        }
+
+        private void btnConnectAndGetTableNames_Click(object sender, EventArgs e)
+        {
+            string error = "";
+
+            if (!checkAllFields())
+            {
+                MessageBox.Show("Lütfen gerekli alanları doldurunuz.");
+                return;
+            }
+
+
+            error = ConnectToDatabase();
+            if (error != "")
+            {
+                MessageBox.Show("Veritabanına bağlanılamadı.\n" + error);
+                return;
+            }
+
+
+            if (chkYedekle.Checked)
+            {
+                DialogResult dialogResult = MessageBox.Show("Bağlantı başarılı\nYedekleme işlemi başlatılsın mı?", "BAŞARILI", MessageBoxButtons.OKCancel);
+
+                if (dialogResult == DialogResult.Cancel)
+                {
+                    return;
+                }
+
+                error = BackupAllDatabases();
+                if (error != "")
+                {
+                    MessageBox.Show("Yedek alınamadı.\n" + error);
+                    return;
+                }
+
+                MessageBox.Show("Yedekleme başarılı.");
+            }
+            else if (chkYedektenGeriYukle.Checked)
+            {
+                DialogResult dialogResult = MessageBox.Show("Bağlantı başarılı\nYedekten geri yükleme işlemi başlatılsın mı?", "BAŞARILI", MessageBoxButtons.OKCancel);
+
+                if (dialogResult == DialogResult.Cancel)
+                {
+                    return;
+                }
+
+                error = GetFileListFromDirectory();
+                if (error != "")
+                {
+                    MessageBox.Show("Yedek listesi alınamadı.\n" + error);
+                }
+                for (int i = 0; i < fileNameList.Count; i++)
+                {
+                    string createDbSQL = string.Format("Create database '{0}'", fileNameList[i]);
+                    OleDbCommand cmd = new OleDbCommand(createDbSQL, cnn);
+                    cmd.ExecuteNonQuery();
+
+                }
+            }
+        }
+
+        private bool checkAllFields()
+        {
+            if (txtServerName.Text == "" || txtUserName.Text == "" || txtPassword.Text == "")
+            {
+                return false;
+            }
+
+            if (!Directory.Exists(txtBackupPath.Text))
+            {
+                return false;
+            }
+            return true;
+        }
+
+        private string BackupAllDatabases()
+        {
+            string error = "";
+            string backupAllDatabasesQuery = string.Format("DECLARE @name VARCHAR(50)" + "\n" +
+                                        "DECLARE @path VARCHAR(256)" + "\n" +
+                                        "DECLARE @fileName VARCHAR(256)" + "\n" +
+                                        "DECLARE @fileDate VARCHAR(20)" + "\n" +
+                                        "SET @path = '{0}'" + "\n" +
+                                        "SELECT @fileDate = CONVERT(VARCHAR(20),GETDATE(),112)" + "\n" +
+                                        "DECLARE db_cursor CURSOR FOR" + "\n" +
+                                        "SELECT name" + "\n" +
+                                        "FROM master.dbo.sysdatabases" + "\n" +
+                                        "WHERE name NOT IN ('master','model','msdb','tempdb')" + "\n" +
+                                        "OPEN db_cursor" + "\n" +
+                                        "FETCH NEXT FROM db_cursor INTO @name" + "\n" +
+                                        "WHILE @@FETCH_STATUS = 0" + "\n" +
+                                        "BEGIN" + "\n" +
+                                        "SET @fileName = @path + @name + '-_-' + @fileDate + '.bak'" + "\n" +
+                                        "BACKUP DATABASE @name TO DISK = @fileName" + "\n" +
+                                        "FETCH NEXT FROM db_cursor INTO @name" + "\n" +
+                                        "END" + "\n" +
+                                        "CLOSE db_cursor" + "\n" +
+                                        "DEALLOCATE db_cursor", txtBackupPath.Text);
+
+            try
+            {
+                OleDbCommand cmd = new OleDbCommand(backupAllDatabasesQuery, cnn);
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception e)
+            {
+                error = e.Message;
+            }
+            return error;
+        }
+
+        private string ConnectToDatabase()
+        {
+            string error = "";
+
+            string connetionString = string.Format("Provider=SQLOLEDB;Server={0};Database=master;User Id={1};Password={2};",
+                txtServerName.Text, txtUserName.Text, txtPassword.Text);
+
+            cnn = new OleDbConnection(connetionString);
+            try
+            {
+                cnn.Open();
+            }
+            catch (Exception ex)
+            {
+                error = ex.Message;
+            }
+            return error;
+        }
+
+        private void chkYedekle_CheckedChanged(object sender, EventArgs e)
+        {
+            if (chkYedekle.Checked == true)
+            {
+                btnConnectAndGetTableNames.Text = "BAĞLAN VE YEDEKLE";
+            }
+            else
+            {
+                btnConnectAndGetTableNames.Text = "BAĞLAN VE YEDEKTEN GERİ YÜKLE";
+            }
+
+            chkYedektenGeriYukle.Checked = !chkYedekle.Checked;
+        }
+
+        private void chkYedektenGeriYukle_CheckedChanged(object sender, EventArgs e)
+        {
+            chkYedekle.Checked = !chkYedektenGeriYukle.Checked;
+        }
+
+        private void btnGozat_Click(object sender, EventArgs e)
+        {
+            fBrwDialog.ShowDialog();
+            string mPath = fBrwDialog.SelectedPath;
+            if (mPath != "")
+            {
+                if (mPath.Substring(mPath.Length - 1) != @"\")
+                {
+                    mPath += @"\";
+                }
+                txtBackupPath.Text = mPath;
+            }
+        }
+
+    }
+}
